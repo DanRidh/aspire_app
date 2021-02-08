2a356cb751ed3905e3ead200ad0b803c8335c907
joannetan <tanjoanne128@gmail.com>
2021-02-08 18:49:40 +0800

    added props to be passed to card

--- src/containers/ClassCard.js
+++ src/containers/ClassCard.js
@@ -1,28 +1,29 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import clsx from "clsx";
+// import clsx from "clsx";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
-import Avatar from "@material-ui/core/Avatar";
+// import Collapse from "@material-ui/core/Collapse";
+// import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
+// import FavoriteIcon from "@material-ui/icons/Favorite";
 import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+// import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import image from "../images/card-image.jpg";
-import { Button } from "@material-ui/core";
-
-// Work In Progress //
+import { Avatar, Button } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 345,
+    width: 345,
+    minHeight:450,
+    margin:10
+    
   },
   media: {
     height: 0,
@@ -43,38 +44,47 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ClassCard({
-  profilePhoto,
-  username,
-  title,
-  description,
-}) {
-  const classes = useStyles({ profilePhoto, username });
+export default function ClassCard({c}) {
+  // const classes = useStyles({ profilePhoto, username });
+  const classes = useStyles();
+
+  // TO FIX: Timezone in UTC+00:00
+  // to change to GMT+08:00
+  const start = new Date(c.start_time)
+  const startDate= start.toLocaleDateString('en-MY',{timeZone:'Asia/Singapore'})
+  const startTime = start.toLocaleTimeString('en-MY',{timeZone:'Asia/Singapore'})
+  console.log(startTime)
+
+  const end = new Date (c.end_time)
+  const endDate = end.toLocaleDateString()
+  const endTime = end.toLocaleTimeString()
+  console.log(startTime)
 
   return (
     <Card className={classes.root}>
-      <CardMedia
-        className={classes.media}
-        image={image}
-        title="Paella dish"
-        style={{ marginTop: "20px" }}
-      />
 
       <CardHeader
-        // action={
-        //   <IconButton aria-label="settings">
-        //     <MoreVertIcon />
-        //   </IconButton>
-        // }
-        title={`${title}`}
-        subheader={`by ${username}`}
+        avatar = {
+          <Avatar src={c.tutor.profile_image}></Avatar>
+        }
+        title={`${c.title}`}
+        subheader={`by ${c.tutor.first_name} ${c.tutor.last_name}`}
       />
+        <CardMedia
+          className={classes.media}
+          image={image}
+          title= {c.title}
+          style={{ marginTop: "20px" }}
+        />
       <CardContent>
+        <Typography>Category: {c.subject.category}</Typography>
         <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like. {description}
+          {c.description}
         </Typography>
+        <Typography>Start: {startDate} {startTime}</Typography>
+        <Typography>End: {endDate} {endTime}</Typography>
+        
+        
       </CardContent>
       <CardActions>
         <Button>Enroll</Button>
