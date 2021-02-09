4ab4cd53a883a4517456406cea1387ddf4580e1f
Dan Ridh <danialridh1@gmail.com>
2021-02-09 16:17:55 +0800

    added axios post request for payment success

--- src/components/StripeButton.js
+++ src/components/StripeButton.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { Button } from '@material-ui/core';
+import axios from "axios";
 
 const StripeButton = ({c}) => {
 
   const onToken = (token) => {
     console.log(token);
+    console.log("payment success")
+    axios({
+      method: 'POST',
+      url: 'https://aspire-api2021.herokuapp.com/api/v1/payments/update',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`
+      },
+      data: {
+        payment_id: `${token.id}`
+      } 
+    })
+    .then((res) => {
+      console.log(res);
+    }, (err) => {
+      console.log(err);
+    }); 
   };
+
 
   return (
     <>
@@ -17,7 +36,9 @@ const StripeButton = ({c}) => {
         currency="MYR"
         allowRememberMe
         stripeKey="pk_test_51IIPpEDb9urO8ySSNFVPAxyXayE8NPrjmkrBckdIrw51XYDdh8qFas3GDuZ4wS7rHm54LK77wRihFtZxluveAZyS00Ddoaf1Gv"
-      />
+      >
+        <Button variant="contained" color="secondary">Make Payment</Button>
+      </StripeCheckout>
     </>
   );
 };
